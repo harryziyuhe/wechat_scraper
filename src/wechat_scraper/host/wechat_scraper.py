464f3706e95a743553d39530dbb37cb4b283231f
hfheziyu <zih028@ucsd.edu>
2024-09-26 12:41:52 -0500

    Move wechat_scraper.py

--- src/wechat_scraper/host/wechat_scraper.py
+++ src/wechat_scraper/host/wechat_scraper.py
@@ -9,70 +9,76 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import random
 
-date_time_pattern = re.compile(r'var create_time = "(\d+)"')
-global_params = None
-account_name = ''
-offset = 0
-count = 0
-df_article = None
-continue_flag = 1
+DATE_TIME_PATTERN = re.compile(r'var create_time = "(\d+)"')
+GLOBAL_PARAMS = None
+ACCOUNT_NAME = ''
+OFFSET = 0
+COUNT = 0
+DF_ARTICLE = None
+CONTINUE_FLAG = 1
 urllib3.disable_warnings()
 params_path = os.path.join(os.path.dirname(__file__), '../virtualbox/params.json')
 log_path = os.path.join(os.path.dirname(__file__), 'log.json')
 offset_path = os.path.join(os.path.dirname(__file__), 'offset.json')
+PASSWORD = 'secret'
+VERBOSE = False
 
 def get_params(reload = False):
-    global global_params
-    global account_name
+    global GLOBAL_PARAMS
+    global ACCOUNT_NAME
+    global VERBOSE
     if os.path.exists(params_path):
         os.remove(params_path)
     if reload:
-        refresh(account_name)
+        refresh(ACCOUNT_NAME)
+    if VERBOSE:
+        print('Detecting...')
     while True:
         if os.path.exists(params_path):
             json_params = open(params_path, 'r', encoding = 'utf-8')
             params = json.load(json_params)
-            global_params = UserData.fromJson(params)
+            GLOBAL_PARAMS = UserData.fromJson(params)
             json_params.close()
-            if global_params != None:
+            if GLOBAL_PARAMS != None:
                 print('Parameters detected')
                 break
-        print('Detecting')
         time.sleep(1)
 
 def initialize():
-    global offset
-    global count
-    global global_params
-    global account_name
-    offset = 0
-    count = 0
+    global OFFSET
+    global COUNT
+    global GLOBAL_PARAMS
+    global ACCOUNT_NAME
+    OFFSET = 0
+    COUNT = 0
     if os.path.exists(params_path):
         os.remove(params_path)
     get_params()
-    account_name = get_account_name(global_params)
-    print(f'Start scraping {account_name}')
+    ACCOUNT_NAME = get_account_name(GLOBAL_PARAMS)
+    print(f'Start scraping {ACCOUNT_NAME}')
 
 
 def get_links(user: UserData, tor = True):
-    global continue_flag
-    url = f'https://mp.weixin.qq.com/mp/profile_ext?action=getmsg&f=json&count=10&is_ok=1&__biz={user.biz}&key={user.key}&uin={user.uin}&pass_ticket={user.pass_ticket}&offset={offset}'
-    session = get_tor_session(tor)
-    msg_json = session.get(url, headers=user_head(global_params), verify=False).json()
+    global CONTINUE_FLAG
+    global PASSWORD
+    url = f'https://mp.weixin.qq.com/mp/profile_ext?action=getmsg&f=json&count=10&is_ok=1&__biz={user.biz}&key={user.key}&uin={user.uin}&pass_ticket={user.pass_ticket}&offset={OFFSET}'
+    session = get_tor_session(tor, PASSWORD)
+    msg_json = session.get(url, headers=user_head(GLOBAL_PARAMS), verify=False).json()
     if (('can_msg_continue' in msg_json) and msg_json['can_msg_continue'] != 1):
-        continue_flag = 0
+        CONTINUE_FLAG = 0
     if 'general_msg_list' in msg_json.keys():
         url_list = json.loads(msg_json['general_msg_list'])['list']
         return url_list
     return []
 
 def check_existing(article_detail: ArticleData):
-    global df_article
-    return((df_article['title'] == article_detail.title).any())
+    global DF_ARTICLE
+    return((DF_ARTICLE['title'] == article_detail.title).any())
 
 def parse_entry(user: UserData, article_detail: ArticleData, entry, tor = True):
-    global offset
-    global count
+    global OFFSET
+    global COUNT
+    global VERBOSE
     if 'app_msg_ext_info' in entry:
         entry = entry['app_msg_ext_info']
         #print(entry)
@@ -80,22 +86,24 @@ def parse_entry(user: UserData, article_detail: ArticleData, entry, tor = True):
         if flag == 'LinkError':
             print(f'Link Error: {entry}')
         if flag == 'Scraped':
+            if VERBOSE:
             print('Some articles have been scraped')
             return flag
-        count += 1
+        COUNT += 1
         if 'multi_app_msg_item_list' in entry:
             sublist = entry['multi_app_msg_item_list']
-            print('Sub-articles detected')
+            if VERBOSE:
+                print('Sub-articles detected')
             for item in sublist:
                 flag = get_article(user, article_detail, item, tor)
                 if flag == 'LinkError':
                     print(f'Link Error: {item}')
-                count += 1
+                COUNT += 1
     else:
-        print('No title')
+        print('Scrape Error. No title')
 
 def get_article(user: UserData, article_detail: ArticleData, entry, tor = True):
-    global df_article
+    global DF_ARTICLE
     if 'title' in entry:
         article_detail.title = entry['title']
         article_detail.link = entry['content_url'].replace('amp;', '')
@@ -112,11 +120,12 @@ def get_article(user: UserData, article_detail: ArticleData, entry, tor = True):
         get_stats(article_detail, user, tor)
     except Exception as e:
         print(f'Scrape Stats Error. Title: {entry['title']}, link: {entry['content_url'].replace('amp;', '')}, error message: {e}')
-    df_article = pd.concat([df_article, pd.DataFrame([vars(article_detail)])], ignore_index=True)
+    DF_ARTICLE = pd.concat([DF_ARTICLE, pd.DataFrame([vars(article_detail)])], ignore_index=True)
     time.sleep(random.uniform(2,5))
     
 def get_content(article_detail: ArticleData, tor = True):
-    session = get_tor_session(tor)
+    global PASSWORD
+    session = get_tor_session(tor, PASSWORD)
     response = session.get(article_detail.link, headers=general_head, verify=False, timeout=(10, 10))
     soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -132,14 +141,12 @@ def get_content(article_detail: ArticleData, tor = True):
     if article_text:
         article_text = article_text.get_text()
         content = '\n'.join([text.strip() for text in article_text if text.strip()])
-    else:
-        print('No element was found.')
     
     article_detail.content = content
 
     # get article published date
     create_time = ''
-    match = date_time_pattern.search(response.text)
+    match = DATE_TIME_PATTERN.search(response.text)
     if match:
         timestamp = int(match.group(1))
         # Convert the timestamp to a datetime object
@@ -147,7 +154,8 @@ def get_content(article_detail: ArticleData, tor = True):
     article_detail.pub_time = create_time
 
 def get_stats(article_detail: ArticleData, user: UserData, tor = True):
-    session = get_tor_session(tor)
+    global PASSWORD
+    session = get_tor_session(tor, PASSWORD)
     read_num, like_num = 0, 0
     query_params = parse_qs(urlparse(article_detail.link).query)
     mid = query_params['mid'][0]
@@ -158,20 +166,19 @@ def get_stats(article_detail: ArticleData, user: UserData, tor = True):
                              data=article_data(user, mid, sn, idx), verify=False).json()
     if 'appmsgstat' in response:
         info = response['appmsgstat']
-        print(info)
         read_num = info['read_num']
         like_num = info['old_like_num']
         article_detail.read = read_num
         article_detail.like = like_num
     article_detail.scrape_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-def run(tor = True, day_max = 2000):
-    global global_params
-    global offset
-    global count
-    global df_article
-    global continue_flag
-    global account_name
+def run(tor = True, daymax = 2000):
+    global GLOBAL_PARAMS
+    global OFFSET
+    global COUNT
+    global DF_ARTICLE
+    global CONTINUE_FLAG
+    global ACCOUNT_NAME
     start_time = time.time()
     initialize()
     
@@ -194,42 +201,42 @@ def run(tor = True, day_max = 2000):
     with open(offset_path, 'r', encoding='utf-8') as json_file:
         offset_dict = json.load(json_file)
     
-    if account_name not in offset_dict:
-        offset_dict[account_name] = 0
+    if ACCOUNT_NAME not in offset_dict:
+        offset_dict[ACCOUNT_NAME] = 0
         offset_plus = 0
     else:
-        offset_plus = offset_dict[account_name]
+        offset_plus = offset_dict[ACCOUNT_NAME]
 
-    data_path = os.path.join(os.path.dirname(__file__), '../data/{account_name}.csv')
+    data_path = os.path.join(os.path.dirname(__file__), '../data/{ACCOUNT_NAME}.csv')
     
     if os.path.exists(data_path):
-        df_article = pd.read_csv(data_path, index_col=None)
+        DF_ARTICLE = pd.read_csv(data_path, index_col=None)
     else:
-        df_article = pd.DataFrame(columns = ['author', 'title', 'content', 'link', 'read', 'like', 'pub_time', 'scrape_time'])
+        DF_ARTICLE = pd.DataFrame(columns = ['author', 'title', 'content', 'link', 'read', 'like', 'pub_time', 'scrape_time'])
     
     while True:
-        url_list = get_links(global_params, tor)
+        url_list = get_links(GLOBAL_PARAMS, tor)
         # First error detection: automatic refresh
         if len(url_list) == 0:
             print('Reloading parameters, please wait...')
             get_params(reload = True)
-            url_list = get_links(global_params, tor)
+            url_list = get_links(GLOBAL_PARAMS, tor)
             # Second error detection: manual refresh
             if len(url_list) == 0:
                 input('Awaiting user action. Press any key to continue.')
                 get_params()
-                url_list = get_links(global_params, tor)
+                url_list = get_links(GLOBAL_PARAMS, tor)
                 # Third error detection: break
                 if len(url_list) == 0:
                     ('Parameter error, verify account status')
-                    print(f'Article collection ended. {count} articles collected.')
+                    print(f'Article collection ended. {COUNT} articles collected.')
                     break
         first_overlap = True
         for entry in url_list:
             article_detail = ArticleData()
             flag = ''
             try:
-                flag = parse_entry(global_params, article_detail, entry, tor)
+                flag = parse_entry(GLOBAL_PARAMS, article_detail, entry, tor)
             except Exception as e:
                 print(e)
                 print(f'Scrape Error: {entry}')
@@ -237,28 +244,46 @@ def run(tor = True, day_max = 2000):
                 if first_overlap:
                     first_overlap = False
                 else:
-                    offset = offset + offset_plus - 1
+                    OFFSET = OFFSET + offset_plus - 1
                     if offset_plus > 2:
                         offset_plus = 2
                         break
                     else: 
                         continue
-            offset += 1
-            df_article.to_csv(data_path, index=False)
-            offset_dict[account_name] = offset
+            OFFSET += 1
+            DF_ARTICLE.to_csv(data_path, index=False)
+            offset_dict[ACCOUNT_NAME] = OFFSET
             with open(offset_path, 'w') as json_file:
                 json.dump(offset_dict, json_file)
-        if (scrape_log['scrape_count'] > day_max):
-            print(f'Article collection completed. {count} articles collected.')
+        if (scrape_log['scrape_count'] > daymax):
+            print(f'Article collection completed. {COUNT} articles collected.')
             break
-        if (continue_flag == 0):
-            print(f'Article collection completed. {count} articles collected.')
+        if (CONTINUE_FLAG == 0):
+            print(f'Article collection completed. {COUNT} articles collected.')
             break
-        print(f'Time elapsed: {(time.time() - start_time):.2f} seconds, {count} articles scraped')
-        scrape_log['scrape_count'] = start_count + count
+        if VERBOSE:
+            print(f'Time elapsed: {(time.time() - start_time):.2f} seconds, {COUNT} articles scraped')
+        scrape_log['scrape_count'] = start_count + COUNT
         with open(log_path, 'w') as log_file:
             json.dump(scrape_log, log_file)
         time.sleep(random.uniform(2, 5))
+
+def wechat_scraper(verbose = False, daymax = 2500):
+    """
+    Scrape WeChat data.
+    
+    Parameters:
+    verbose (bool): If True, enable verbose mode. Default is False.
+    daymax (int): Maximum number of days to scrape. Default is 2500.
+    """
+    global VERBOSE
+    VERBOSE = verbose
+    tor = input('Use Tor to avoid IP blocks (default is yes)? Y/n: ').upper()
+    if (tor == 'N') or (tor == 'No'):
+        tor = False
+    else:
+        tor = True
+    run(tor, daymax)
 
 ##################################################################
 # Pipeline 
@@ -269,11 +294,5 @@ def run(tor = True, day_max = 2000):
 # 5. After each iteration, save article data into csv file for maximum record retention
 
 if __name__ == '__main__':
-    directory = os.getcwd()
-    tor = input('Use Tor to avoid IP blocks (default is yes)? Y/n: ').upper()
-    if (tor == 'N') or (tor == 'No'):
-        tor = False
-    else:
-        tor = True
-    run(tor)
+    wechat_scraper()
     
